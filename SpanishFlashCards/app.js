362163585d4ea421f604f79ee0d11e71ef7f3a7d
Holliewood67 <54012195+Holliewood67@users.noreply.github.com>
2021-02-19 18:13:06 -0600

    Add files via upload

--- SpanishFlashCards/app.js
+++ SpanishFlashCards/app.js
@@ -1,12 +1,32 @@
-var firstButton = document.getElementsByClassName('card-button')[0];
-var firstAnswer = document.getElementsByClassName('card-answer')[0];
+// var firstButton = document.getElementsByClassName('card-button')[0];
+// var firstAnswer = document.getElementsByClassName('card-answer')[0];
 
-firstButton.addEventListener('click', function(event) {
-    if (firstButton.innerHTML == 'Show') {
-        firstAnswer.style.display = 'block';
-        firstButton.innerHTML = 'Hide';
-    } else if (firstButton.innerHTML == 'Hide') {
-        firstAnswer.style.display = 'none';
-        firstButton.innerHTML = 'Show';
-    }
-})
+// firstButton.addEventListener('click', function(event) {
+    // if (firstButton.innerHTML == 'Show') {
+    //     firstAnswer.style.display = 'block';
+    //     firstButton.innerHTML = 'Hide';
+    // } else if (firstButton.innerHTML == 'Hide') {
+    //     firstAnswer.style.display = 'none';
+    //     firstButton.innerHTML = 'Show';
+    // }
+// })
+
+var cards = document.getElementsByClassName('card');
+
+for (var cardIndex in cards) {
+    var button = cards[cardIndex].querySelector('.card-button');
+    button.addEventListener('click', function(event){
+        var currentButton = event.target;
+        var currentCard = currentButton.parentNode;
+        var currentAnswer = currentCard.querySelector('.card-answer');
+
+        if (currentButton.innerHTML == 'Show') {
+            currentAnswer.style.display = 'block';
+            currentButton.innerHTML = 'Hide';
+        } else if (currentButton.innerHTML == 'Hide') {
+            currentAnswer.style.display = 'none';
+            currentButton.innerHTML = 'Show';
+        }
+    
+    })
+}
